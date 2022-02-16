b3828160270d61a842f777028c3f1d1de4a72077
samuel santos <samaraujo28@gmail.com>
2022-02-16 20:23:11 -0300

    Setimo requisito atendido

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
+const orderedList = document.querySelector('#lista-tarefas');
+
 function addTask() {
   const button = document.querySelector('#criar-tarefa');
 
   button.addEventListener('click', () => {
-    const orderedList = document.querySelector('#lista-tarefas');
     const input = document.querySelector('#texto-tarefa');
     const li = document.createElement('li');
 
+    li.classList.add('item');
     li.innerHTML = input.value;
     orderedList.appendChild(li);
     input.value = '';
   });
 }
 
+function changeBackgroundColor() {
+  
+  orderedList.addEventListener('click', (event) => {
+    const element = event.target;
+    const hasClassNameItem = element.className.includes('item');
+
+    if (hasClassNameItem) {
+      element.style.backgroundColor = 'grey';
+    }
+  });
+}
+
+changeBackgroundColor();
 addTask();
